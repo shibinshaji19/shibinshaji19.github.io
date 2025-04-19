d990cfff63766a37018e5a52ee8c74ad185060d7
Shibin Shaji <shibin.shaji@dcmail.ca>
2025-04-18 21:55:46 -0400

    Added necessary accessibility features to webpage

--- Assignment - 5/assessment-files/main.js
+++ Assignment - 5/assessment-files/main.js
@@ -1,4 +1,3 @@
-// functionality for showing/hiding the comments section
 
 const showHideBtn = document.querySelector('.show-hide');
 const commentWrapper = document.querySelector('.comment-wrapper');
@@ -16,8 +15,6 @@ showHideBtn.onclick = function() {
   }
 };
 
-// functionality for adding a new comment via the comments form
-
 const form = document.querySelector('.comment-form');
 const nameField = document.querySelector('#name');
 const commentField = document.querySelector('#comment');
@@ -30,18 +27,57 @@ form.onsubmit = function(e) {
 
 function submitComment() {
   const listItem = document.createElement('li');
+  const nameLabel = document.createElement('span');
+  const commentLabel = document.createElement('span');
   const namePara = document.createElement('p');
   const commentPara = document.createElement('p');
   const nameValue = nameField.value;
   const commentValue = commentField.value;
 
-  namePara.textContent = nameValue;
-  commentPara.textContent = commentValue;
+  namePara.setAttribute("tabindex", "0");
+  commentPara.setAttribute("tabindex", "0");
+
+  nameLabel.textContent = "Author: ";
+  commentLabel.textContent = "Comment: ";
+
+  namePara.appendChild(nameLabel);
+  commentPara.appendChild(commentLabel);
+  
+  namePara.innerHTML += nameValue;
+  commentPara.innerHTML += commentValue;
+
+  console.log("attr: ", namePara.getAttribute("tabindex"));
 
   list.appendChild(listItem);
   listItem.appendChild(namePara);
   listItem.appendChild(commentPara);
 
+  console.log("namePara: ", namePara);
+
   nameField.value = '';
   commentField.value = '';
 }
+  console.log("Script started");
+
+  const transcriptSection = document.querySelector('.transcript');
+  const transcriptToggleBtn = document.querySelector('.transcript-container button');
+
+  transcriptToggleBtn.addEventListener('click', () => {
+  const isHidden = transcriptToggleBtn.textContent === 'Show transcript';
+  transcriptSection.style.height = isHidden ? '150px' : '0';
+  transcriptToggleBtn.textContent = isHidden ? 'Hide transcript' : 'Show transcript';
+});
+
+  const commentToggleBtn = document.querySelector('.show-hide');
+
+  commentToggleBtn.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    event.target.click();
+  }
+});
+
+  const skipLink = document.querySelector('#skip-to-content-link');
+
+  skipLink.addEventListener('focus', () => {
+  skipLink.classList.toggle('skip-to-content-link-visible');
+});
